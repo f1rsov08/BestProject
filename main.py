d02051ad047164422a76caae9c33c71b598b6267
f1rsov08 <122812064+f1rsov08@users.noreply.github.com>
2023-10-31 19:49:04 +0300

    Добавлены редактирование и удаление приказов

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 import sqlite3
-
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidgetItem, QMessageBox
+from PyQt5.QtCore import QDate
 
 
 class MainWindow(QMainWindow):
@@ -16,12 +16,14 @@ class MainWindow(QMainWindow):
                              'По организации': lambda x: x[5],
                              'По дате': lambda x: x[3],
                              'По приказывающему': lambda x: x[8]}
-        self.table = []
+        self.table = {}
         uic.loadUi('mainWidget.ui', self)
 
         self.addButton.clicked.connect(self.add)
-        self.pushButton.clicked.connect(self.filter)
-        self.editButton.clicked.connect(self.edit)
+        self.filterButton.clicked.connect(self.filter)
+        self.searchButton.clicked.connect(self.update_table)
+        self.listWidget.itemDoubleClicked.connect(self.edit)
+        self.update_table()
 
     def add(self):
         self.addWindow = AddWindow(self)
@@ -31,17 +33,16 @@ class MainWindow(QMainWindow):
         self.filterWindow = FilterWindow(self)
         self.filterWindow.show()
 
-    def edit(self):
-        self.editWindow = EditWindow(self)
+    def edit(self, item):
+        self.editWindow = EditWindow(self, item)
         self.editWindow.show()
 
     def update_table(self):
         try:
-            self.table = list(self.cur.execute(f"""SELECT * FROM orders {self.filt}""").fetchall())
+            self.table = {i[0]: i[1:] for i in self.cur.execute(f"""SELECT * FROM orders {self.filt}""")}
             self.listWidget.clear()
-            self.table.sort(key=self.sorting_dict[self.comboBox.currentText()])
-            for i in self.table:
-                self.listWidget.addItem(i[1])
+            for i in sorted(self.table.keys()):
+                self.listWidget.addItem(i)
         except Exception as e:
             print(str(e))
 
@@ -59,10 +60,22 @@ class AddWindow(QWidget):
         uic.loadUi('addWidget.ui', self)
         self.saveButton.clicked.connect(self.save)
         self.cancelButton.clicked.connect(self.close)
-        self.addButton.clicked.connect(self.addRow)
-        self.deleteButton.clicked.connect(self.delRow)
+        self.addRowButton.clicked.connect(self.addRow)
+        self.delRowButton.clicked.connect(self.delRow)
         self.guysTable.itemSelectionChanged.connect(
-            lambda: self.deleteButton.setEnabled(len(self.guysTable.selectedItems()) != 0))
+            lambda: self.delRowButton.setEnabled(len(self.guysTable.selectedItems()) != 0))
+        self.name.textChanged.connect(self.check_input)
+
+    def check_input(self):
+        if self.name.text() in self.main_window.table.keys():
+            self.saveButton.setDisabled(True)
+            self.statusbar.setText('Название уже занято')
+        elif not self.name.text():
+            self.saveButton.setDisabled(True)
+            self.statusbar.setText('Название не может быть пустым')
+        else:
+            self.saveButton.setEnabled(True)
+            self.statusbar.setText('')
 
     def save(self):
         try:
@@ -73,19 +86,21 @@ class AddWindow(QWidget):
             msg.setIcon(QMessageBox.Warning)
             msg.setWindowTitle("Пустые ячейки")
             msg.setText("Пустые ячейки в приказывающих")
-            msg.exec_()
-            return
-        self.main_window.cur.execute(f"INSERT INTO orders(name,date,number,organization,title,reason,text,guys) VALUES"
-                                     f"('{self.name.text()}', "
-                                     f"'{self.date.date().toString('dd-MM-yyyy')}', "
-                                     f"'{self.number.text()}', "
-                                     f"'{self.organization.text()}', "
-                                     f"'{self.title.text()}', "
-                                     f"'{self.reason.toPlainText()}', "
-                                     f"'{self.text.toPlainText()}', "
-                                     f"'{guys}')")
-        self.main_window.update_table()
-        self.close()
+        else:
+            reason, text = self.reason.toPlainText().replace('\n', '\\n'), self.text.toPlainText().replace('\n', '\\n')
+            self.main_window.cur.execute(
+                f"INSERT INTO orders(name,date,number,organization,title,reason,text,guys) VALUES"
+                f"('{self.name.text()}', "
+                f"'{self.date.date().toString('dd-MM-yyyy')}', "
+                f"'{self.number.text()}', "
+                f"'{self.organization.text()}', "
+                f"'{self.title.text()}', "
+                f"'{reason}', "
+                f"'{text}', "
+                f"'{guys}')")
+            self.main_window.con.commit()
+            self.main_window.update_table()
+            self.close()
 
     def addRow(self):
         self.guysTable.setRowCount(self.guysTable.rowCount() + 1)
@@ -99,9 +114,90 @@ class AddWindow(QWidget):
 
 
 class EditWindow(QWidget):
-    def __init__(self):
+    def __init__(self, main_window, item):
         super().__init__()
+        self.item = item
+        self.main_window = main_window
         uic.loadUi('editWidget.ui', self)
+        self.saveButton.clicked.connect(self.save)
+        self.delRowButton.clicked.connect(self.delete)
+        self.cancelButton.clicked.connect(self.close)
+        self.addRowButton.clicked.connect(self.addRow)
+        self.deleteButton.clicked.connect(self.delete)
+        self.guysTable.itemSelectionChanged.connect(
+            lambda: self.delRowButton.setEnabled(len(self.guysTable.selectedItems()) != 0))
+        self.name.textChanged.connect(self.check_input)
+
+        self.name.setText(self.item.text())
+        day, month, year = map(int, self.main_window.table[self.item.text()][0].split('-'))
+        self.date.setDate(QDate(year, month, day))
+        self.number.setValue(int(self.main_window.table[self.item.text()][1]))
+        self.title.setText(self.main_window.table[self.item.text()][2])
+        self.organization.setText(self.main_window.table[self.item.text()][3])
+        self.reason.setPlainText(self.main_window.table[self.item.text()][4].replace('\\n', '\n'))
+        self.text.setPlainText(self.main_window.table[self.item.text()][5].replace('\\n', '\n'))
+
+        self.guysTable.setRowCount(len(self.main_window.table[self.item.text()][6].split(',')))
+        for i, row in enumerate(self.main_window.table[self.item.text()][6].split(',')):
+            for j, elem in enumerate(row.split(':')):
+                self.guysTable.setItem(i, j, QTableWidgetItem(elem))
+
+    def check_input(self):
+        if self.name.text() in self.main_window.table.keys() and self.name.text() != self.item.text():
+            self.saveButton.setDisabled(True)
+            self.statusbar.setText('Название уже занято')
+        elif not self.name.text():
+            self.saveButton.setDisabled(True)
+            self.statusbar.setText('Название не может быть пустым')
+        else:
+            self.saveButton.setEnabled(True)
+            self.statusbar.setText('')
+
+    def save(self):
+        try:
+            guys = ','.join([f'{self.guysTable.item(i, 0).text()}:{self.guysTable.item(i, 1).text()}'
+                             for i in range(self.guysTable.rowCount())])
+        except AttributeError:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setWindowTitle("Пустые ячейки")
+            msg.setText("Пустые ячейки в приказывающих")
+        else:
+            reason, text = self.reason.toPlainText().replace('\n', '\\n'), self.text.toPlainText().replace('\n', '\\n')
+            self.main_window.cur.execute(
+                f"UPDATE orders "
+                f"SET name = '{self.name.text()}', "
+                f"date = '{self.date.date().toString('dd-MM-yyyy')}', "
+                f"number = '{self.number.text()}', "
+                f"organization = '{self.organization.text()}', "
+                f"title = '{self.title.text()}', "
+                f"reason = '{reason}', "
+                f"text = '{text}', "
+                f"guys = '{guys}' "
+                f"WHERE name = '{self.item.text()}'")
+            self.main_window.con.commit()
+            self.main_window.update_table()
+            self.close()
+
+    def delete(self):
+        valid = QMessageBox.question(
+            self, '', f"Вы действительно хотите безвозвратно удалить {self.item.text()}?",
+            QMessageBox.Yes, QMessageBox.No)
+        if valid == QMessageBox.Yes:
+            self.main_window.cur.execute(f"DELETE from orders where name = '{self.item.text()}'")
+            self.main_window.con.commit()
+            self.main_window.update_table()
+            self.close()
+
+    def addRow(self):
+        self.guysTable.setRowCount(self.guysTable.rowCount() + 1)
+        for i in range(2):
+            self.guysTable.setItem(self.guysTable.rowCount(), i, QTableWidgetItem())
+
+    def delRow(self):
+        selected_items = self.guysTable.selectedItems()
+        for item in selected_items:
+            self.guysTable.removeRow(item.row())
 
 
 if __name__ == '__main__':
